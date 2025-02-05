31d1bcddf7764b25ba2f4e3d76c1cba6995a8c6b
Paulo Lorenzo <pldlorenzo@gmail.com>
2025-02-05 15:27:40 +0800

    Changed data in schema, fixed grammar
    Signed-off-by: Paulo Lorenzo <pldlorenzo@gmail.com>

--- Base/accounts/src/main/java/com/pdlorenzo/accounts/controller/AccountsController.java
+++ Base/accounts/src/main/java/com/pdlorenzo/accounts/controller/AccountsController.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @Tag(
-        name = "CRUD REST APIs for Accounts in EazyBank",
-        description = "CRUD REST APIs in EazyBank to CREATE, UPDATE, FETCH AND DELETE account details"
+        name = "CRUD REST APIs for Accounts in PDL Bank",
+        description = "CRUD REST APIs in PDL Bank to CREATE, UPDATE, FETCH AND DELETE account details"
 )
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -38,7 +38,7 @@ public class AccountsController {
 
     @Operation(
             summary = "Create Account REST API",
-            description = "REST API to create new Customer &  Account inside EazyBank"
+            description = "REST API to create new Customer &  Account inside PDL Bank"
     )
     @ApiResponses({
             @ApiResponse(
